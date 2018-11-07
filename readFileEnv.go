9395f6c4c082d7f7a35e83c8d92b7ca069396ef4
Harold Schreckengost <harold@haroldmschreckengost.com>
2018-11-07 08:35:38 -0600

    Oops.

--- readFileEnv.go
+++ readFileEnv.go
@@ -1,13 +1,14 @@
 package readFileEnv
 
 import (
+	"fmt"
 	"io/ioutil"
 	"strings"
 )
 
 func MakeEnvFileReader(dir string) func(file string) string {
 	return func(file string) string {
-		fileData, err := ioutil.ReadFile(file)
+		fileData, err := ioutil.ReadFile(fmt.Sprintf("%s/%s", dir, file))
 		if err != nil {
 			panic(err.Error())
 		}
